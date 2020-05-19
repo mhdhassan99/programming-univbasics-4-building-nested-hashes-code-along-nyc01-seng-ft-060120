f81d312ebdddcbaf57d29cda16711833c5a505f9
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-19 22:41:25 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,17 +1,18 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
+ epic_tragedy = {
+   :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => {}
-      }, 
-    :capulet => {
+      :hero_friends => []
+   },
+   :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => {}
-      }}
+      :heroine_friends => []
+   }
+}
 
   
 end
